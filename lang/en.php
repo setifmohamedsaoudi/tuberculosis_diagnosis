f7674ebcc87dc7a7292327f1932870b33ffd4e25
setifmohamedsaoudi <setifmohamedsaoudi@gmail.com>
2024-10-13 18:23:36 +0100

    Create en.php

---
+++ lang/en.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'title' => 'Tuberculosis Diagnosis',
+    'select_language' => 'Select Language:',
+    'english' => 'English',
+    'arabic' => 'Arabic',
+    'french' => 'French',
+    'enter' => 'Enter',
+    'permalink' => 'Permalink',
+    'adolescents_definition' => 'Adolescents are defined in this chapter as patients aged 10 years and older.',
+    'signs_symptoms' => 'Signs and Symptoms of Pulmonary Tuberculosis (PTB)',
+    'hiv_positive' => 'In case of HIV infection, one of the following signs: cough, fever, weight loss, or night sweats.',
+    'hiv_negative' => 'In the absence of HIV infection, one of the following signs: cough lasting more than 2 weeks, cough with hemoptysis, unexplained weight loss, night sweats, or clinical suspicion.',
+    'lf_lam' => 'LF-LAM',
+    'perform_lf_lam' => 'Perform the lateral flow urine lipoarabinomannan test (LF-LAM) only in patients infected with HIV.',
+    'perform_rmt' => 'Regardless of the LF-LAM result, also perform a rapid molecular test (RMT) because:',
+    'rmt_positive_if_lf_lam_negative' => 'A RMT could be positive (better sensitivity) if the LF-LAM is negative.',
+    'rmt_detect_resistance' => 'A RMT also allows detection of rifampicin resistance.',
+    // Add more translations as needed...
+];
+?>
