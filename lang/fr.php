564bc67a52bf3d0c348429ffdd730bfd3b53e1c9
setifmohamedsaoudi <setifmohamedsaoudi@gmail.com>
2024-10-13 18:22:57 +0100

    Create fr.php

---
+++ lang/fr.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'title' => 'Diagnostic de Tuberculose',
+    'select_language' => 'Sélectionnez la langue:',
+    'english' => 'Anglais',
+    'arabic' => 'Arabe',
+    'french' => 'Français',
+    'enter' => 'Entrer',
+    'permalink' => 'Lien permanent',
+    'adolescents_definition' => 'Les adolescents sont définis dans ce chapitre comme les patients de 10 ans et plus.',
+    'signs_symptoms' => 'Signes et symptômes de la tuberculose pulmonaire (TBP)',
+    'hiv_positive' => 'En cas d\'infection par le VIH, l\'un des signes suivants : toux, fièvre, perte de poids ou sueurs nocturnes.',
+    'hiv_negative' => 'En l\'absence d\'infection par le VIH, l\'un des signes suivants : toux de plus de 2 semaines, toux avec hémoptysie, perte de poids inexpliquée, sueurs nocturnes ou suspicion clinique.',
+    'lf_lam' => 'LF-LAM',
+    'perform_lf_lam' => 'Réaliser le test urinaire de lipoarabinomannane à flux latéral (LF-LAM) uniquement chez les patients infectés par le VIH.',
+    'perform_rmt' => 'Quel que soit le résultat du LF-LAM, réaliser aussi un test moléculaire rapide (TMR) car :',
+    'rmt_positive_if_lf_lam_negative' => 'Un TMR pourrait être positif (meilleure sensibilité) si le LF-LAM est négatif.',
+    'rmt_detect_resistance' => 'Un TMR permet en plus de détecter une résistance à la rifampicine.',
+    // Add more translations as needed...
+];
+?>
